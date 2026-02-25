d26ea258c0eb4cacf86c85277de8447e2c5b358b
Om Patil <112097632+kushpo357@users.noreply.github.com>
2026-02-25 12:10:09 +0530

    Collection View address bar

---
+++ addressBar/addressBar/AddressCardCell.swift
@@ -0,0 +1,40 @@
+//
+//  AddressCardCell.swift
+//  addressBar
+//
+//  Created by Om Patil on 24/02/26.
+//
+
+import UIKit
+class AddressCardCell : UICollectionViewCell {
+    
+    static let identifier: String = "AddressCardCell"
+    private let addressCard = AddressCardView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+private extension AddressCardCell {
+    
+    func setupUI() {
+        contentView.addSubview(addressCard)
+        addressCard.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+                addressCard.topAnchor.constraint(equalTo: contentView.topAnchor),
+                addressCard.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+                addressCard.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+                addressCard.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            ])
+    }
+}
--- addressBar/addressBar/ViewController.swift
+++ addressBar/addressBar/ViewController.swift
@@ -9,131 +9,78 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let cardView = UIView()
-    let dividerView = UIView()
+    private let collectionView : UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        
+        layout.scrollDirection = .vertical
+        layout.minimumInteritemSpacing = 16
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.register(AddressCardCell.self, forCellWithReuseIdentifier: "AddressCardCell")
+        return collectionView
+    }()
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
     
-    //Labels
-    let titleLabel = UILabel()
-    let addressLabel = UILabel()
-    let nameLabel = UILabel()
-    let numberLabel = UILabel()
-    let seperatorLabel1 = UILabel()
-    let seperatorLabel2 = UILabel()
-    let homeLabel = UILabel()
-    
-    //Buttons
-    let deleteButton = UIButton()
-    let editButton = UIButton()
-    
-    //views
-    let detailStack = UIStackView()
-    let infoStack = UIStackView()
-    let buttonStack = UIStackView()
-    let mainStack = UIStackView()
-    
-    
+    required init?(coder: NSCoder) {
+        super.init(nibName: nil, bundle: nil)
+    }
+
     override func viewDidLoad() {
+        // your setup goes here
         super.viewDidLoad()
-        self.view.backgroundColor = .systemBackground
         setupUI()
-        setupContraints()
+        setupConstraints()
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
     }
+    
+    
 }
 
 private extension ViewController {
-    func setupUI() {
-        cardView.translatesAutoresizingMaskIntoConstraints = false
-        cardView.backgroundColor = .white
-        cardView.layer.cornerRadius = 16
-        cardView.layer.shadowColor = UIColor.black.cgColor
-        cardView.layer.shadowOpacity = 0.1
-        cardView.layer.shadowOffset = .zero
-        cardView.layer.shadowRadius = 16
-        self.view.addSubview(cardView)
-        
-        mainStack.axis = .vertical
-        mainStack.distribution = .fill
-        mainStack.spacing = 8
-        mainStack.translatesAutoresizingMaskIntoConstraints = false
-        cardView.addSubview(mainStack)
-        
-        dividerView.backgroundColor = .systemGray4
-        dividerView.translatesAutoresizingMaskIntoConstraints = false
-        
-        titleLabel.text = "The most common way to do it is, The only thing you have in mind is"
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        titleLabel.layer.shadowOpacity = 0.1
-        titleLabel.textColor = .black
-        titleLabel.numberOfLines = 0
-        
-        addressLabel.text = "16 Cambridge Road, 16, Cambridge Rd, Halasuru, Cambridg Layout, Jogupalya, Bengaluru, Karnataka 560058, India"
-        addressLabel.translatesAutoresizingMaskIntoConstraints = false
-        addressLabel.layer.shadowOpacity = 0.1
-        addressLabel.textColor = .systemGray2
-        addressLabel.numberOfLines = 0
-        
-        nameLabel.text = "👤 My Name"
-        nameLabel.translatesAutoresizingMaskIntoConstraints = false
-        numberLabel.text = "2123000008"
-        numberLabel.translatesAutoresizingMaskIntoConstraints = false
-        homeLabel.translatesAutoresizingMaskIntoConstraints = false
-        homeLabel.text = "HOME"
-        seperatorLabel1.text = "|"
-        seperatorLabel1.translatesAutoresizingMaskIntoConstraints = false
-        
-        
-        seperatorLabel2.text = "|"
-        seperatorLabel2.translatesAutoresizingMaskIntoConstraints = false
-        
-        detailStack.axis = .horizontal
-        detailStack.distribution = .equalSpacing
-        [nameLabel, seperatorLabel1, numberLabel, seperatorLabel2, homeLabel].forEach { detailStack.addArrangedSubview($0) }
-        infoStack.distribution = .fill
-        infoStack.axis = .vertical
-        
-        [titleLabel, addressLabel, detailStack].forEach {
-            infoStack.addArrangedSubview($0)
-        }
-        
-        deleteButton.setTitle("Delete", for: .normal)
-        deleteButton.setTitleColor(.black, for: .normal)
-        deleteButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
-
-        editButton.setTitle("Edit", for: .normal)
-        editButton.setTitleColor(.black, for: .normal)
-        editButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
-        
-        buttonStack.axis = .horizontal
-        buttonStack.distribution = .fill
-        buttonStack.spacing = 16
-        buttonStack.alignment = .center
-        
-        let spacer = UIView()
-        spacer.setContentHuggingPriority(.defaultLow, for: .horizontal)
-        buttonStack.addArrangedSubview(spacer)
-        
-        [deleteButton, editButton].forEach { buttonStack.addArrangedSubview($0) }
-        [infoStack, dividerView, buttonStack].forEach { mainStack.addArrangedSubview($0) }
+    
+    func setupUI(){
+        view.addSubview(collectionView)
+        collectionView.dataSource = self
+        collectionView.delegate = self
     }
     
-    func setupContraints(){
+    func setupConstraints() {
         NSLayoutConstraint.activate([
-            cardView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 16),
-            cardView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -16),
-            cardView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
-            
-            mainStack.topAnchor.constraint(equalTo: cardView.topAnchor, constant: 16),
-            mainStack.leadingAnchor.constraint(equalTo: cardView.leadingAnchor, constant: 16),
-            mainStack.trailingAnchor.constraint(equalTo: cardView.trailingAnchor, constant: -16),
-            mainStack.bottomAnchor.constraint(equalTo: cardView.bottomAnchor, constant: -16),
-            dividerView.heightAnchor.constraint(equalToConstant: 1)
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
     }
 }
 
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "AddressCardCell", for: indexPath) as? AddressCardCell else {
+            return UICollectionViewCell()
+        }
+        return cell
+    }
+}
 
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: (collectionView.bounds.width - 32), height: 400)
+    }
+    
+    
+}
 
