ace6d4488058b3c1ab9b215aa6076f292e427b78
Om Patil <112097632+kushpo357@users.noreply.github.com>
2026-03-23 11:53:47 +0530

    added search bar : customised tabBar, searchBar UI

--- BookStoreApp/BookStoreApp/BookViewModel.swift
+++ BookStoreApp/BookStoreApp/BookViewModel.swift
@@ -5,6 +5,8 @@
 //  Created by Om Patil on 17/03/26.
 //
 
+import Foundation
+
 protocol BookViewModelDelegate: AnyObject {
 }
 
@@ -14,10 +16,17 @@ class BookViewModel {
     private(set) var wishlistBooks = [Book]()
     var selectedSort: SortOption?
     private(set) var chips: [String] = ["All"] + BookTag.allCases.map { $0.rawValue }
+    var searchQuery: String = ""
     
     var displayedBooks: [Book] {
         let filtered = bookStore.books.filter { Set(selectedTags).isSubset(of: Set($0.tags)) }
-        guard let sort = selectedSort else { return filtered }
-        return sort.apply(to: filtered)
-     }
+        
+        let searched = searchQuery.isEmpty ? filtered : filtered.filter {
+            $0.title.localizedCaseInsensitiveContains(searchQuery) ||
+            $0.author.localizedCaseInsensitiveContains(searchQuery)
+        }
+        
+        guard let sort = selectedSort else { return searched }
+        return sort.apply(to: searched)
+    }
 }
